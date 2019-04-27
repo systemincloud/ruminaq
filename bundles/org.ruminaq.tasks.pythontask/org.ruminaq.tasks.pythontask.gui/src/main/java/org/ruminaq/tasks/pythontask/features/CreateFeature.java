5a3ae8b7f374b3b6249878ef86cc95334dc2e7d8
Marek Jagielski <marek.jagielski@gmail.com>
2019-04-27 18:35:36 +0200

    progress

--- bundles/org.ruminaq.tasks.pythontask/org.ruminaq.tasks.pythontask.gui/src/main/java/org/ruminaq/tasks/pythontask/features/CreateFeature.java
+++ bundles/org.ruminaq.tasks.pythontask/org.ruminaq.tasks.pythontask.gui/src/main/java/org/ruminaq/tasks/pythontask/features/CreateFeature.java
@@ -9,17 +9,47 @@ import org.ruminaq.tasks.TaskCategory;
 import org.ruminaq.tasks.features.CreateTaskFeature;
 import org.ruminaq.tasks.pythontask.Images;
 import org.ruminaq.tasks.pythontask.Port;
+import org.ruminaq.tasks.pythontask.model.pythontask.PythonTask;
+import org.ruminaq.tasks.pythontask.model.pythontask.PythontaskFactory;
 
 public class CreateFeature extends CreateTaskFeature {
 
-	public CreateFeature(IFeatureProvider fp, String bundleName, Version version) {	super(fp, PythonTask.class, bundleName, version); }
+	public CreateFeature(IFeatureProvider fp, String bundleName, Version version) {
+		super(fp, PythonTask.class, bundleName, version);
+	}
 
-	@Override public String getPaletteKey()       { return SicPlugin.GUI_ID.s(); }
-	@Override public String getTestPaletteKey()   { return SicPlugin.GUI_ID.s(); }
-	@Override public String getTaskCategory()     { return TaskCategory.USERDEFINED.name(); }
-	@Override public String getTestTaskCategory() { return TaskCategory.USERDEFINED.name(); }
+	@Override
+	public String getPaletteKey() {
+		return SicPlugin.GUI_ID.s();
+	}
 
-	@Override public    Object[]                    create(ICreateContext context) { return super.create(context, PythontaskFactory.eINSTANCE.createPythonTask()); }
-	@Override protected Class<? extends PortsDescr> getPortsDescription()          { return Port.class; }
-	@Override public    String                      getCreateImageId()             { return Images.K.IMG_PYTHONTASK_PALETTE.name(); }
+	@Override
+	public String getTestPaletteKey() {
+		return SicPlugin.GUI_ID.s();
+	}
+
+	@Override
+	public String getTaskCategory() {
+		return TaskCategory.USERDEFINED.name();
+	}
+
+	@Override
+	public String getTestTaskCategory() {
+		return TaskCategory.USERDEFINED.name();
+	}
+
+	@Override
+	public Object[] create(ICreateContext context) {
+		return super.create(context, PythontaskFactory.eINSTANCE.createPythonTask());
+	}
+
+	@Override
+	protected Class<? extends PortsDescr> getPortsDescription() {
+		return Port.class;
+	}
+
+	@Override
+	public String getCreateImageId() {
+		return Images.K.IMG_PYTHONTASK_PALETTE.name();
+	}
 }
