69a9f2a6d70fa654838a1c125b53fcdbb7992580
Marek Jagielski <marek.jagielski@gmail.com>
2019-05-03 16:22:14 +0200

    progress

---
+++ tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/package-info.java
@@ -0,0 +1,11 @@
+/**
+* Domain classes used to produce .....
+* <p>
+* These classes contain the ......
+* </p>
+*
+* @since 1.0
+* @author somebody
+* @version 1.0
+*/
+package org.ruminaq.eclipse.it.tests;
