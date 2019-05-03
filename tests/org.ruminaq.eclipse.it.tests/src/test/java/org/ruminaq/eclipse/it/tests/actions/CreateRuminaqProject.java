e63c7bfc82f1ca4f1c5bd0c7934555e6f80e6465
Marek Jagielski <marek.jagielski@gmail.com>
2019-05-03 20:27:18 +0200

    progress

--- tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/actions/CreateRuminaqProject.java
+++ tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/actions/CreateRuminaqProject.java
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 package org.ruminaq.eclipse.it.tests.actions;
 
 import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
@@ -10,26 +11,37 @@ import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 
+/**
+ * Create new Ruminaq project.
+ *
+ * @author Marek Jagielski
+ *
+ */
 public class CreateRuminaqProject {
 
-    public void execute(SWTWorkbenchBot bot, String name) {
-        bot.menu("File").menu("New").menu("Project...").click();
+  /**
+   * Create new Ruminaq project.
+   *
+   * @param bot SWTWorkbenchBot
+   * @param name name of project
+   */
+  public void execute(SWTWorkbenchBot bot, String name) {
+    bot.menu("File").menu("New").menu("Project...").click();
 
-        bot.tree().getTreeItem("Ruminaq").expand();
+    bot.tree().getTreeItem("Ruminaq").expand();
 
-        bot.tree().getTreeItem("Ruminaq").getNode("Ruminaq Project").select();
+    bot.tree().getTreeItem("Ruminaq").getNode("Ruminaq Project").select();
 
-        bot.button("Next >").click();
+    bot.button("Next >").click();
 
-        bot.textWithLabel("&Project name:").setText(name);
+    bot.textWithLabel("&Project name:").setText(name);
 
-        bot.button("Finish").click();
+    bot.button("Finish").click();
 
-        SWTBotShell shellOpenPerpeitve = bot
-                .shell("Open Associated Perspective?");
-        shellOpenPerpeitve.activate();
-        bot.button("Open Perspective").click();
+    SWTBotShell shellOpenPerpeitve = bot.shell("Open Associated Perspective?");
+    shellOpenPerpeitve.activate();
+    bot.button("Open Perspective").click();
 
-        bot.waitUntil(shellCloses(shellOpenPerpeitve));
-    }
+    bot.waitUntil(shellCloses(shellOpenPerpeitve));
+  }
 }
