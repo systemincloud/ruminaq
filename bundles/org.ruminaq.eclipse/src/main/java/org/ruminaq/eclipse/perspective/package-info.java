8d5b4ea44447c37409ae76658162f92300fbf758
Marek Jagielski <marek.jagielski@gmail.com>
2019-06-10 00:17:04 +0200

    progress

--- bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/perspective/package-info.java
+++ bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/perspective/package-info.java
@@ -4,4 +4,4 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
 
-package org.ruminaq.eclipse.it.tests.api;
+package org.ruminaq.eclipse.perspective;
