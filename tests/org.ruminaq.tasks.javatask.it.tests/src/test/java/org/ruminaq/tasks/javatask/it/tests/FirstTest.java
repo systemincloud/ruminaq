6d05360a695a65c63c3a5112fe8efbd87b44130b
Marek Jagielski <marek.jagielski@gmail.com>
2019-06-28 23:25:41 +0200

    progress

--- tests/org.ruminaq.tasks.javatask.it.tests/src/test/java/org/ruminaq/tasks/javatask/it/tests/FirstTest.java
+++ tests/org.ruminaq.tasks.javatask.it.tests/src/test/java/org/ruminaq/tasks/javatask/it/tests/FirstTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 
 class FirstTest {
 
-    @Test
-    void test() {
-        System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-//        fail("Not yet implemented");
-    }
+	@Test
+	void test() {
+	}
 
 }
