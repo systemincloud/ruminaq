20e4ffc66401811eb320e2afebfaceb6b58d9d14
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-12 22:55:02 +0200

    progress

---
+++ bundles/org.ruminaq.tasks.randomgenerator/org.ruminaq.tasks.randomgenerator.gui/src/main/java/org/ruminaq/tasks/randomgenerator/EclipseExtensionImpl.java
@@ -0,0 +1,14 @@
+package org.ruminaq.tasks.randomgenerator;
+
+import org.osgi.service.component.annotations.Component;
+import org.ruminaq.eclipse.api.EclipseExtension;
+import org.ruminaq.tasks.randomgenerator.model.randomgenerator.RandomgeneratorPackage;
+
+@Component(property = { "service.ranking:Integer=10" })
+public class EclipseExtensionImpl implements EclipseExtension {
+
+  @Override
+  public void initEditor() {
+  	RandomgeneratorPackage.eINSTANCE.getClass();
+  }
+}
