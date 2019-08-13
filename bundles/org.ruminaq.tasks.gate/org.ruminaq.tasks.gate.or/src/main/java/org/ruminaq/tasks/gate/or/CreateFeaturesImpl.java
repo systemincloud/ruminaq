ac04e19fa908cf0f579e6f9030ba03519ba947d4
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-13 19:04:34 +0200

    progress

---
+++ bundles/org.ruminaq.tasks.gate/org.ruminaq.tasks.gate.or/src/main/java/org/ruminaq/tasks/gate/or/CreateFeaturesImpl.java
@@ -0,0 +1,58 @@
+package org.ruminaq.tasks.gate.or;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.graphiti.features.ICreateFeature;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICreateContext;
+import org.osgi.service.component.annotations.Component;
+import org.ruminaq.gui.api.CreateFeaturesExtension;
+import org.ruminaq.gui.features.create.PaletteCreateFeature;
+import org.ruminaq.gui.palette.CommonPaletteCompartmentEntry;
+import org.ruminaq.model.desc.PortsDescr;
+import org.ruminaq.tasks.features.CreateTaskFeature;
+import org.ruminaq.tasks.gate.Port;
+import org.ruminaq.tasks.gate.or.model.or.Or;
+import org.ruminaq.tasks.gate.or.model.or.OrFactory;
+
+@Component(property = { "service.ranking:Integer=10" })
+public class CreateFeaturesImpl implements CreateFeaturesExtension {
+
+	@Override
+	public List<Class<? extends ICreateFeature>> getFeatures() {
+		return Arrays.asList(CreateFeature.class);
+	}
+	
+	public static class CreateFeature extends CreateTaskFeature implements PaletteCreateFeature {
+
+		public CreateFeature(IFeatureProvider fp) {
+			super(fp, Or.class);
+		}
+
+		@Override
+		public String getCompartment() {
+			return CommonPaletteCompartmentEntry.DEFAULT_COMPARTMENT;
+		}
+		
+		@Override
+		public String getStack() {
+			return CommonPaletteCompartmentEntry.LOGIC_STACK;
+		}
+
+		@Override
+		public Object[] create(ICreateContext context) {
+			return super.create(context, OrFactory.eINSTANCE.createOr());
+		}
+
+		@Override
+		protected Class<? extends PortsDescr> getPortsDescription() {
+			return Port.class;
+		}
+
+		@Override
+		public String getCreateImageId() {
+			return Images.K.IMG_OR_PALETTE.name();
+		}
+	}
+}
