d68b1dfe0622f0a16ff591ece35727983333d897
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-13 18:53:41 +0200

    progress

---
+++ bundles/org.ruminaq.tasks.embeddedtask/src/main/java/org/ruminaq/tasks/embeddedtask/ContextButtonPadTool.java
@@ -0,0 +1,37 @@
+package org.ruminaq.tasks.embeddedtask;
+
+import java.util.function.Predicate;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.IPictogramElementContext;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.osgi.service.component.annotations.Component;
+import org.ruminaq.consts.Constants;
+import org.ruminaq.gui.api.GenericContextButtonPadDataExtension;
+import org.ruminaq.model.ruminaq.EmbeddedTask;
+import org.ruminaq.tasks.embeddedtask.ContextButtonPadTool.Filter;
+import org.ruminaq.util.ServiceFilter;
+import org.ruminaq.util.ServiceFilterArgs;
+
+@Component(property = { "service.ranking:Integer=10" })
+@ServiceFilter(Filter.class)
+public class ContextButtonPadTool implements GenericContextButtonPadDataExtension {
+
+	static class Filter implements Predicate<ServiceFilterArgs> {
+
+		@Override
+		public boolean test(ServiceFilterArgs args) {
+			IFeatureProvider fp = (IFeatureProvider) args.getArgs().get(0);
+			IPictogramElementContext context = (IPictogramElementContext) args
+			    .getArgs().get(1);
+			PictogramElement pe = context.getPictogramElement();
+			return fp
+			    .getBusinessObjectForPictogramElement(pe) instanceof EmbeddedTask;
+		}
+	}
+	
+	@Override
+	public int getGenericContextButtons() {
+		return Constants.CONTEXT_BUTTON_DELETE | Constants.CONTEXT_BUTTON_UPDATE;
+	}
+}
