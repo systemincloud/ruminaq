7748e6f633b040329e202c44c03e65dfcd269131
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-13 18:58:46 +0200

    progress

---
+++ bundles/org.ruminaq.tasks.gate/org.ruminaq.tasks.gate.not/src/main/java/org/ruminaq/tasks/gate/not/CreateFeaturesImpl.java
@@ -0,0 +1,60 @@
+package org.ruminaq.tasks.gate.not;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.graphiti.features.ICreateFeature;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICreateContext;
+import org.osgi.service.component.annotations.Component;
+import org.ruminaq.gui.api.CreateFeaturesExtension;
+import org.ruminaq.gui.features.create.PaletteCreateFeature;
+import org.ruminaq.gui.palette.CommonPaletteCompartmentEntry;
+import org.ruminaq.model.desc.PortsDescr;
+import org.ruminaq.tasks.gate.not.impl.Port;
+import org.ruminaq.tasks.features.CreateTaskFeature;
+import org.ruminaq.tasks.gate.not.model.not.Not;
+import org.ruminaq.tasks.gate.not.model.not.NotFactory;
+
+@Component(property = { "service.ranking:Integer=10" })
+public class CreateFeaturesImpl implements CreateFeaturesExtension {
+
+	@Override
+	public List<Class<? extends ICreateFeature>> getFeatures() {
+		return Arrays.asList(CreateFeature.class);
+	}
+	
+	public static class CreateFeature extends CreateTaskFeature implements PaletteCreateFeature {
+
+		public CreateFeature(IFeatureProvider fp) {
+			super(fp, Not.class);
+		}
+
+		@Override
+		public String getCompartment() {
+			return CommonPaletteCompartmentEntry.DEFAULT_COMPARTMENT;
+		}
+		
+		@Override
+		public String getStack() {
+			return CommonPaletteCompartmentEntry.LOGIC_STACK;
+		}
+
+		@Override
+		public Object[] create(ICreateContext context) {
+			Not gt = NotFactory.eINSTANCE.createNot();
+			gt.setInputNumber(1);
+			return super.create(context, gt);
+		}
+
+		@Override
+		protected Class<? extends PortsDescr> getPortsDescription() {
+			return Port.class;
+		}
+
+		@Override
+		public String getCreateImageId() {
+			return Images.K.IMG_NOT_PALETTE.name();
+		}
+	}
+}
