e78dbdc4d6d33436390e2a49ffb72b04f6e061ac
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-15 23:48:53 +0200

    progress

--- bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/RuminaqPerspective.java
+++ bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/RuminaqPerspective.java
@@ -25,10 +25,15 @@ public class RuminaqPerspective implements IPerspectiveFactory {
   private static final String ID_MARKERS_VIEW = "org.eclipse.ui.views.AllMarkersView";
   private static final String SHORTCUT_FOLDER = "org.eclipse.ui.wizards.new.folder";
   private static final String SHORTCUT_FILE = "org.eclipse.ui.wizards.new.file";
+  private static final String LAUNCH_COOLBAR_SHORTCUT = "org.eclipse.debug.ui.launchActionSet";
 
   private static final String TOP_LEFT_ID = "topLeft";
   private static final String BOTTOM_LEFT_ID = "bottomLeft";
   private static final String BOTTOM_RIGHT_ID = "bottomRight";
+
+  private static final float TOP_LEFT_RATIO = 0.26f;
+  private static final float BOTTOM_LEFT_RATIO = 0.50f;
+  private static final float BOTTOM_RIGHT_RATIO = 0.66f;
 
   /**
    * Creates the initial layout for a page.
@@ -44,7 +49,7 @@ public class RuminaqPerspective implements IPerspectiveFactory {
     defineLayout(layout);
   }
 
-  private void defineActions(IPageLayout layout) {
+  private static void defineActions(IPageLayout layout) {
 
     // Add "new wizards".
     layout.addNewWizardShortcut(SHORTCUT_FOLDER);
@@ -62,23 +67,23 @@ public class RuminaqPerspective implements IPerspectiveFactory {
     layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
 
     layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
-    layout.addActionSet("org.eclipse.debug.ui.launchActionSet");
+    layout.addActionSet(LAUNCH_COOLBAR_SHORTCUT);
   }
 
-  private void defineLayout(IPageLayout layout) {
+  private static void defineLayout(IPageLayout layout) {
     String editorArea = layout.getEditorArea();
 
     IFolderLayout topLeft = layout.createFolder(TOP_LEFT_ID, IPageLayout.LEFT,
-        (float) 0.26, editorArea);
+        TOP_LEFT_RATIO, editorArea);
     topLeft.addView(IPageLayout.ID_PROJECT_EXPLORER);
     topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 
     IFolderLayout bottomLeft = layout.createFolder(BOTTOM_LEFT_ID,
-        IPageLayout.BOTTOM, (float) 0.50, TOP_LEFT_ID);
+        IPageLayout.BOTTOM, BOTTOM_LEFT_RATIO, TOP_LEFT_ID);
     bottomLeft.addView(ThumbNailView.VIEW_ID);
 
     IFolderLayout bottomRight = layout.createFolder(BOTTOM_RIGHT_ID,
-        IPageLayout.BOTTOM, (float) 0.66, editorArea);
+        IPageLayout.BOTTOM, BOTTOM_RIGHT_RATIO, editorArea);
 
     bottomRight.addView(IPageLayout.ID_PROP_SHEET);
     bottomRight.addView(ID_MARKERS_VIEW);
