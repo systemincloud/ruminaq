e59c80f971440acdb05a830170297215a1cd1ac6
Marek Jagielski <marek.jagielski@gmail.com>
2019-08-16 03:09:18 +0200

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/ImageProvider.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/ImageProvider.java
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
 
-package org.ruminaq.gui.providers;
+package org.ruminaq.gui;
 
 import java.util.Collection;
 import java.util.Map;
