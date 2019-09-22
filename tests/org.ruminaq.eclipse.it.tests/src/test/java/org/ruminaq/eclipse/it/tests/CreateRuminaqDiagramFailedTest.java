ec55e9a8ccef262cbad7b1e4cc8a5ab809594c56
Marek Jagielski <marek.jagielski@gmail.com>
2019-09-22 13:34:45 +0200

    progress

--- tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/CreateRuminaqDiagramFailedTest.java
+++ tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/CreateRuminaqDiagramFailedTest.java
@@ -6,12 +6,16 @@
 
 package org.ruminaq.eclipse.it.tests;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ruminaq.eclipse.wizards.project.SourceFolders;
+import org.ruminaq.tests.common.CreateRuminaqDiagram;
+import org.ruminaq.tests.common.CreateRuminaqProject;
 import org.ruminaq.tests.common.SelectView;
 
 /**
@@ -43,20 +47,20 @@ public class CreateRuminaqDiagramFailedTest {
 
   @Test
   public final void testCreateDiagramFailed() {
-//    String projectName = "test"
-//        + RandomStringUtils.randomAlphabetic(SUFFIX_LENGTH);
-//
-//    new CreateRuminaqProject().execute(bot, projectName);
-//    new CreateRuminaqProject().acceptPerspectiveChangeIfPopUps(bot);
-//
-//    String path = SourceFolders.DIAGRAM_FOLDER;
-//
-//    String diagramName = "Diagram_"
-//        + RandomStringUtils.randomAlphabetic(SUFFIX_LENGTH);
+    String projectName = "test"
+        + RandomStringUtils.randomAlphabetic(SUFFIX_LENGTH);
+
+    new CreateRuminaqProject().execute(bot, projectName);
+    new CreateRuminaqProject().acceptPerspectiveChangeIfPopUps(bot);
+
+    String path = SourceFolders.DIAGRAM_FOLDER;
+
+    String diagramName = "Diagram_"
+        + RandomStringUtils.randomAlphabetic(SUFFIX_LENGTH);
 //    System.setProperty(
 //        CreateDiagramWizardAspect.FAIL_OPEN_EDITOR_FILE_NAME,
 //        diagramName);
-//    new CreateRuminaqDiagram().execute(bot, projectName, path, diagramName);
+    new CreateRuminaqDiagram().execute(bot, projectName, path, diagramName);
 
 //    SWTBotShell failureWindow = bot.shell("Ruminaq failure");
 //    failureWindow.activate();
