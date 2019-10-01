a61609cc8bdfa4f02344e99603b85fa35b1cb690
Marek Jagielski <marek.jagielski@gmail.com>
2019-10-02 01:02:28 +0200

    progress

---
+++ tests/org.ruminaq.eclipse.it.tests/src/test/java/org/ruminaq/eclipse/it/tests/ChangeProjectPropertiesTest.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
+package org.ruminaq.eclipse.it.tests;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.finders.ContextMenuHelper;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ruminaq.tests.common.CreateRuminaqProject;
+import org.ruminaq.tests.common.SelectView;
+
+/**
+ * Test of creating a new eclipse project.
+ *
+ * @author Marek Jagielski
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class ChangeProjectPropertiesTest {
+
+  private static SWTWorkbenchBot bot;
+
+  /**
+   * Initialize SWTBot.
+   *
+   */
+  @BeforeClass
+  public static void initBot() {
+    bot = new SWTWorkbenchBot();
+    SelectView.closeWelcomeViewIfExists(bot);
+  }
+
+  @AfterClass
+  public static void after() {
+    bot.resetWorkbench();
+  }
+
+  private static final int PROJECT_SUFFIX_LENGTH = 5;
+
+  @Test
+  public final void testChangeProjectProperties() {
+    String projectName = "test"
+        + RandomStringUtils.randomAlphabetic(PROJECT_SUFFIX_LENGTH);
+    new CreateRuminaqProject().execute(bot, projectName);
+    new CreateRuminaqProject().acceptPerspectiveChangeIfPopUps(bot);
+
+    SWTBotTree selector = SelectView.selectInProjectExplorer(bot, projectName,
+        new String[0]);
+    SWTBotMenu menu = new SWTBotMenu(ContextMenuHelper.contextMenu(selector,
+        new String[] { "Properties" }));
+    menu.click();
+    bot.tree().getTreeItem("Ruminaq").select();
+
+    bot.button("Apply and Close").click();
+  }
+}
