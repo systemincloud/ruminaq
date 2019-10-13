a4f5fe189e95ca13be5d7fc6b1e039e0f4c68eca
Marek Jagielski <marek.jagielski@gmail.com>
2019-10-13 18:25:52 +0200

    progress

--- bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/editor/EditorLinkHelper.java
+++ bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/editor/EditorLinkHelper.java
@@ -6,8 +6,9 @@
 
 package org.ruminaq.eclipse.editor;
 
+import java.util.Optional;
+
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -17,14 +18,13 @@ import org.eclipse.graphiti.ui.editor.DiagramEditorInput;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.navigator.ILinkHelper;
 import org.eclipse.ui.part.FileEditorInput;
 
 /**
- * Provides information to the Navigator on how to
- * link Ruminaq editor with selection of diagram nodes.
+ * Provides information to the Navigator on how to link Ruminaq editor with
+ * selection of diagram nodes.
  *
  * @author Marek Jagielski
  */
@@ -32,49 +32,33 @@ public class EditorLinkHelper implements ILinkHelper {
 
   @Override
   public IStructuredSelection findSelection(IEditorInput editorInput) {
-    if (editorInput instanceof DiagramEditorInput) {
-      DiagramEditorInput diagramEditorInput = (DiagramEditorInput) editorInput;
-      final IFile file = getFile(diagramEditorInput.getUri());
-      if (file != null && editorInput.exists()) {
-        return new StructuredSelection(file);
-      }
-    }
-    return StructuredSelection.EMPTY;
+    return Optional.ofNullable(editorInput).filter(IEditorInput::exists)
+        .filter(ei -> ei instanceof DiagramEditorInput)
+        .map(ei -> (DiagramEditorInput) ei).map(DiagramEditorInput::getUri)
+        .map(uri -> getFile(uri)).map(file -> new StructuredSelection(file))
+        .orElse(StructuredSelection.EMPTY);
   }
 
   @Override
   public void activateEditor(IWorkbenchPage page,
       IStructuredSelection selection) {
-    if (selection == null || selection.isEmpty())
-      return;
-    if (selection.getFirstElement() instanceof IFile) {
-      IEditorInput fileInput = new FileEditorInput(
-          (IFile) selection.getFirstElement());
-      IEditorPart editor = page.findEditor(fileInput);
-      if (editor != null)
-        page.bringToTop(editor);
-    }
+    Optional.ofNullable(selection).filter(s -> !s.isEmpty())
+        .map(IStructuredSelection::getFirstElement)
+        .filter(o -> o instanceof IFile).map(s -> (IFile) s)
+        .map(f -> new FileEditorInput(f)).map(fei -> page.findEditor(fei))
+        .ifPresent(e -> page.bringToTop(e));
   }
 
   private IFile getFile(URI uri) {
-    if (uri == null)
-      return null;
-
-    final IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace()
-        .getRoot();
-
-    final String filePath = getWorkspaceFilePath(uri.trimFragment());
-    if (filePath == null) {
-      final IPath location = Path.fromOSString(uri.toString());
-      final IFile file = workspaceRoot.getFileForLocation(location);
-      return file != null ? file : null;
-    } else {
-      final IResource workspaceResource = workspaceRoot.findMember(filePath);
-      return (IFile) workspaceResource;
-    }
+    IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+    return (IFile) Optional.ofNullable(uri).map(URI::trimFragment)
+        .map(u -> getWorkspaceFilePath(u)).map(fp -> workspaceRoot.findMember(fp)).orElseGet(() -> {
+          IPath location = Path.fromOSString(uri.toString());
+          return workspaceRoot.getFileForLocation(location);
+        });
   }
 
-  private String getWorkspaceFilePath(URI uri) {
+  private static String getWorkspaceFilePath(URI uri) {
     return uri.isPlatform() ? uri.toPlatformString(true) : null;
   }
 }
