bd57222b3de7cae05f1c62049bdd02ae420ae9d5
Marek Jagielski <marek.jagielski@gmail.com>
2019-10-27 20:25:54 +0100

    progress

---
+++ tests/org.ruminaq.eclipse.tests/src/test/java/org/ruminaq/eclipse/tests/RuminaqDiagramUtilTest.java
@@ -0,0 +1,39 @@
+package org.ruminaq.eclipse.tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.emf.common.util.URI;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.ruminaq.eclipse.RuminaqDiagramUtil;
+
+@RunWith(JUnitPlatform.class)
+class RuminaqDiagramUtilTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = { "resource:/src/test/resources/tasks/MyTask.rumi",
+      "resource:/src/test/resources/tasks/dir/MyTask.rumi" })
+  void testIsTestByUri(String input) {
+    URI uri = URI.createFileURI(input);
+    assertTrue("Is test", RuminaqDiagramUtil.isTest(uri));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = { "resource:/src/main/resources/tasks/MyTask.rumi",
+      "resource:/src/test/resources/tasks/dir/MyTask.txt" })
+  void testIsNotTestByUri(String input) {
+    URI uri = URI.createFileURI(input);
+    assertFalse("Isn't test", RuminaqDiagramUtil.isTest(uri));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = { "resource:/project/src/test/resources/tasks/MyTask.rumi" })
+  void testIsTestByUriWithBase(String input) {
+    URI uri = URI.createFileURI(input);
+    assertTrue("Is test", RuminaqDiagramUtil.isTest(uri, "resource:/project/"));
+  }
+
+}
