99d9583abceb14a5131a2ad11803f0421ce1d81c
Marek Jagielski <marek.jagielski@gmail.com>
2019-11-05 21:35:15 +0100

    progress

--- tests/org.ruminaq.gui.it.tests/src/test/java/org/ruminaq/gui/it/tests/AddInputPortTest.java
+++ tests/org.ruminaq.gui.it.tests/src/test/java/org/ruminaq/gui/it/tests/AddInputPortTest.java
@@ -1,10 +1,13 @@
 package org.ruminaq.gui.it.tests;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import static org.junit.Assert.assertFalse;
+
 import org.eclipse.reddeer.common.wait.TimePeriod;
 import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.eclipse.condition.ProjectExists;
 import org.eclipse.reddeer.eclipse.ui.navigator.resources.ProjectExplorer;
+import org.eclipse.reddeer.eclipse.utils.DeleteUtils;
+import org.eclipse.reddeer.gef.editor.GEFEditor;
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
 import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
 import org.junit.After;
@@ -25,8 +28,9 @@ import org.ruminaq.tests.common.reddeer.RuminaqProjectWizard;
 @RunWith(RedDeerSuite.class)
 public class AddInputPortTest {
 
-  private static final int PROJECT_SUFFIX_LENGTH = 5;
-  private static final int DIAGRAM_SUFFIX_LENGTH = 5;
+  private static String projectName = "test";
+
+  private static String diagramName = "Diagram";
 
   @BeforeClass
   public static void maximizeWorkbenchShell() {
@@ -35,37 +39,33 @@ public class AddInputPortTest {
 
   @Before
   public void createProject() {
-    String projectName = "test"
-        + RandomStringUtils.randomAlphabetic(PROJECT_SUFFIX_LENGTH);
     new RuminaqProjectWizard().create(projectName);
     new ProjectExplorer().open();
     new WaitUntil(new ProjectExists(projectName), TimePeriod.MEDIUM, false);
     new ProjectExplorer().getProject(projectName).select();
-    String diagramName = "Diagram_"
-        + RandomStringUtils.randomAlphabetic(DIAGRAM_SUFFIX_LENGTH)
-        + CreateDiagramWizard.DIAGRAM_EXTENSION_DOT;
     new RuminaqDiagramWizard().create(projectName, SourceFolders.DIAGRAM_FOLDER,
-        diagramName);
+        diagramName + CreateDiagramWizard.DIAGRAM_EXTENSION_DOT);
   }
 
   @After
   public void deleteAllProjects() {
-//    new GEFEditor().close();
+    new GEFEditor().close();
     ProjectExplorer projectExplorer = new ProjectExplorer();
     projectExplorer.open();
-//    DeleteUtils.forceProjectDeletion(projectExplorer.getProject(projectName),true);
+    DeleteUtils.forceProjectDeletion(projectExplorer.getProject(projectName),
+        true);
   }
 
   @Test // (expected=TestFailureException.class)
   public void addInputPortTest() {
-//    GEFEditor gefEditor = new GEFEditor("test");
+    GEFEditor gefEditor = new GEFEditor(diagramName);
+    gefEditor.addToolFromPalette("Input Port", 200, 100);
+    assertFalse("Editor is always saved", gefEditor.isDirty());
 //    try {
 //      gefEditor.addToolFromPalette("EClass", 50, 100).setLabel("ClassA");
 //    } catch (CoreLayerException ex) {
 //      throw new TestFailureException(ex.getMessage());
 //    }
-//    gefEditor.addToolFromPalette("EClass", 200, 100).setLabel("ClassB");
-//
 //    new LabeledGraphitiEditPart("ClassA").getContextButton("Delete").click();
 //    new DefaultShell("Confirm Delete").setFocus();
 //    new PushButton("Yes").click();
