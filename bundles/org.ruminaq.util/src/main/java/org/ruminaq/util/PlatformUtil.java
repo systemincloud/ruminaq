7eb51499656b1cfc7372f003b41681a0e8cf6e76
Marek Jagielski <marek.jagielski@gmail.com>
2019-11-06 07:09:41 +0100

    progress

--- bundles/org.ruminaq.util/src/main/java/org/ruminaq/util/PlatformUtil.java
+++ bundles/org.ruminaq.util/src/main/java/org/ruminaq/util/PlatformUtil.java
@@ -1,15 +1,26 @@
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
 package org.ruminaq.util;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.Version;
 
+/**
+ * Eclipse platform utils.
+ *
+ * @author Marek Jagielski
+ */
 public final class PlatformUtil {
 
   private PlatformUtil() {
   }
 
-  public static Bundle getBundle(Class<?> clazz) {
+  private static Bundle getBundle(Class<?> clazz) {
     return FrameworkUtil.getBundle(clazz);
   }
 
