c722d8afff910efa10a46a7a08027bf1c4edd010
Marek Jagielski <marek.jagielski@gmail.com>
2020-02-27 09:05:19 +0100

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/contextmenu/CreateSimpleConnectionPointContextMenu.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/contextmenu/CreateSimpleConnectionPointContextMenu.java
@@ -6,26 +6,27 @@
 
 package org.ruminaq.gui.features.contextmenu;
 
-import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.ICustomContext;
 import org.eclipse.graphiti.features.custom.ICustomFeature;
-import org.eclipse.graphiti.mm.pictograms.FreeFormConnection;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.osgi.service.component.annotations.Component;
 import org.ruminaq.consts.Constants;
 import org.ruminaq.gui.api.ContextMenuEntryExtension;
 import org.ruminaq.gui.features.contextmenu.CreateSimpleConnectionPointContextMenu.Filter;
 import org.ruminaq.gui.features.create.CreateSimpleConnectionPointFeature;
+import org.ruminaq.gui.model.diagram.SimpleConnectionShape;
 import org.ruminaq.gui.model.diagram.impl.GuiUtil;
-import org.ruminaq.model.ruminaq.SimpleConnection;
 import org.ruminaq.util.ServiceFilter;
 import org.ruminaq.util.ServiceFilterArgs;
 
+/**
+ * Service ContextMenuEntryExtension implementation.
+ *
+ * @author Marek Jagielski
+ */
 @Component(property = { "service.ranking:Integer=5" })
 @ServiceFilter(Filter.class)
 public class CreateSimpleConnectionPointContextMenu
@@ -35,15 +36,14 @@ public class CreateSimpleConnectionPointContextMenu
 
     @Override
     public boolean test(ServiceFilterArgs args) {
-      IFeatureProvider fp = (IFeatureProvider) args.getArgs().get(0);
       ICustomContext context = (ICustomContext) args.getArgs().get(1);
       PictogramElement[] pes = context.getPictogramElements();
-      List<Object> bos = Stream.of(pes)
-          .map(pe -> fp.getBusinessObjectForPictogramElement(pe))
-          .collect(Collectors.toList());
-      return bos.size() == 1 && bos.get(0) instanceof SimpleConnection
-          && GuiUtil.distanceToConnection((FreeFormConnection) pes[0],
-              context.getX(), context.getY(), Constants.INTERNAL_PORT) < 5;
+      return pes.length == 1
+          && Stream.of(pes).filter(SimpleConnectionShape.class::isInstance)
+              .map(SimpleConnectionShape.class::cast)
+              .filter(scs -> GuiUtil.distanceToConnection(scs, context.getX(),
+                  context.getY(), Constants.INTERNAL_PORT) < 5)
+              .findFirst().isPresent();
     }
   }
 
