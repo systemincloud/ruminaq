2a2d9a976a8b9f6bd9e8fa4afd466c4034184973
Marek Jagielski <marek.jagielski@gmail.com>
2020-03-21 17:05:05 +0100

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/api/PasteElementFeatureExtension.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/api/PasteElementFeatureExtension.java
@@ -6,8 +6,6 @@
 
 package org.ruminaq.gui.api;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -17,43 +15,36 @@ import java.util.stream.Collectors;
 
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
-import org.ruminaq.gui.features.FeaturePredicate;
 import org.ruminaq.gui.features.PasteFeatureFilter;
 import org.ruminaq.gui.features.paste.RuminaqShapePasteFeature;
 import org.ruminaq.gui.model.diagram.RuminaqShape;
 import org.ruminaq.model.ruminaq.BaseElement;
+import org.ruminaq.util.Result;
 
 /**
  * Service api providing graphiti AddFeature.
  *
  * @author Marek Jagielski
  */
-public interface PasteElementFeatureExtension
-    extends BestFeatureExtension<RuminaqShapePasteFeature<? extends RuminaqShape>> {
+public interface PasteElementFeatureExtension extends
+    BestFeatureExtension<RuminaqShapePasteFeature<? extends RuminaqShape>> {
 
   default List<RuminaqShapePasteFeature<? extends RuminaqShape>> createFeatures(
-      List<Class<? extends RuminaqShapePasteFeature<? extends RuminaqShape>>> features, IFeatureProvider fp,
-      PictogramElement oldPe, int xMin, int yMin) {
+      List<Class<? extends RuminaqShapePasteFeature<? extends RuminaqShape>>> features,
+      IFeatureProvider fp, PictogramElement oldPe, int xMin, int yMin) {
     return Optional.ofNullable(features).orElse(Collections.emptyList())
-        .stream().<Constructor<? extends RuminaqShapePasteFeature<? extends RuminaqShape>>>map(f -> {
-          try {
-            return f.getConstructor(IFeatureProvider.class,
-                PictogramElement.class, Integer.TYPE, Integer.TYPE);
-          } catch (NoSuchMethodException | SecurityException e) {
-            return null;
-          }
-        }).filter(Objects::nonNull).<RuminaqShapePasteFeature<? extends RuminaqShape>>map(c -> {
-          try {
-            return c.newInstance(fp, oldPe, xMin, yMin);
-          } catch (InstantiationException | IllegalAccessException
-              | IllegalArgumentException | InvocationTargetException e) {
-            return null;
-          }
-        }).filter(Objects::nonNull).collect(Collectors.toList());
+        .stream()
+        .map(f -> Result.attempt(() -> f.getConstructor(IFeatureProvider.class,
+            PictogramElement.class, Integer.TYPE, Integer.TYPE)))
+        .map(r -> r.orElse(null)).filter(Objects::nonNull)
+        .map(f -> Result.attempt(() -> f.newInstance(fp, oldPe, xMin, yMin)))
+        .map(r -> r.orElse(null)).filter(Objects::nonNull)
+        .collect(Collectors.toList());
   }
 
-  default RuminaqShapePasteFeature<? extends RuminaqShape> getFeature(IFeatureProvider fp, BaseElement oldBo,
-      PictogramElement oldPe, int xMin, int yMin) {
+  default RuminaqShapePasteFeature<? extends RuminaqShape> getFeature(
+      IFeatureProvider fp, BaseElement oldBo, PictogramElement oldPe, int xMin,
+      int yMin) {
     return createFeatures(getFeatures().stream().filter(filter(oldBo, fp))
         .findFirst().stream().collect(Collectors.toList()), fp, oldPe, xMin,
         yMin).stream().findFirst().orElse(null);
@@ -64,20 +55,10 @@ public interface PasteElementFeatureExtension
     return clazz -> Optional
         .ofNullable(clazz.getAnnotation(PasteFeatureFilter.class))
         .map(PasteFeatureFilter::value)
-        .<Constructor<? extends FeaturePredicate<BaseElement>>>map(f -> {
-          try {
-            return f.getConstructor();
-          } catch (NoSuchMethodException | SecurityException e) {
-            return null;
-          }
-        }).<FeaturePredicate<BaseElement>>map(c -> {
-          try {
-            return c.newInstance();
-          } catch (InstantiationException | IllegalAccessException
-              | IllegalArgumentException | InvocationTargetException e) {
-            return null;
-          }
-        }).orElse(new FeaturePredicate<BaseElement>() {
-        }).test(oldBo, fp);
+        .map(f -> Result.attempt(f::getConstructor))
+        .flatMap(r -> Optional.ofNullable(r.orElse(null)))
+        .map(f -> Result.attempt(f::newInstance))
+        .flatMap(r -> Optional.ofNullable(r.orElse(null)))
+        .map(f -> f.test(oldBo, fp)).orElse(Boolean.TRUE);
   }
 }
