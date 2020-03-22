812c739fbac648aefea440d9f224e059f6d5873b
Marek Jagielski <marek.jagielski@gmail.com>
2020-03-22 14:26:10 +0100

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/contextbuttonpad/ContextButtonPadConnectionPointTool.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/contextbuttonpad/ContextButtonPadConnectionPointTool.java
@@ -6,39 +6,47 @@
 
 package org.ruminaq.gui.features.contextbuttonpad;
 
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import org.eclipse.graphiti.datatypes.IRectangle;
 import org.eclipse.graphiti.features.context.IPictogramElementContext;
-import org.eclipse.graphiti.mm.pictograms.PictogramElement;
-import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.internal.datatypes.impl.RectangleImpl;
 import org.osgi.service.component.annotations.Component;
-import org.ruminaq.consts.Constants;
 import org.ruminaq.gui.api.ContextButtonPadLocationExtension;
 import org.ruminaq.gui.features.contextbuttonpad.ContextButtonPadConnectionPointTool.Filter;
+import org.ruminaq.gui.model.diagram.SimpleConnectionPointShape;
 import org.ruminaq.util.ServiceFilter;
 import org.ruminaq.util.ServiceFilterArgs;
 
+/**
+ * Where to place context pad on SimpleConnectionPointShape.
+ * 
+ * @author Marek Jagielski
+ */
 @Component(property = { "service.ranking:Integer=5" })
 @ServiceFilter(Filter.class)
 public class ContextButtonPadConnectionPointTool
     implements ContextButtonPadLocationExtension {
 
+  private static final int HEIGHT = 80;
+  
   public static class Filter implements Predicate<ServiceFilterArgs> {
 
     @Override
     public boolean test(ServiceFilterArgs args) {
-      IPictogramElementContext context = (IPictogramElementContext) args
-          .getArgs().get(1);
-      PictogramElement pe = context.getPictogramElement();
-      return Boolean.parseBoolean(Graphiti.getPeService().getPropertyValue(pe,
-          Constants.SIMPLE_CONNECTION_POINT));
+      return Optional.ofNullable(args).map(ServiceFilterArgs::getArgs)
+          .map(l -> l.get(1)).filter(IPictogramElementContext.class::isInstance)
+          .map(IPictogramElementContext.class::cast)
+          .map(IPictogramElementContext::getPictogramElement)
+          .filter(SimpleConnectionPointShape.class::isInstance).isPresent();
     }
   }
 
   @Override
   public IRectangle getPadLocation(IRectangle rectangle) {
-    rectangle.setHeight(80);
-    return rectangle;
+    RectangleImpl newRectangle = new RectangleImpl(rectangle);
+    newRectangle.setHeight(HEIGHT);
+    return newRectangle;
   }
 }
