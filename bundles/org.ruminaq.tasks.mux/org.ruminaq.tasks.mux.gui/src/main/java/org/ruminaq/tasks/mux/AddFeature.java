3673d657c47131ed315f136945d5c004f7410cb9
Marek Jagielski <marek.jagielski@gmail.com>
2020-05-08 23:17:33 +0200

    progress

--- bundles/org.ruminaq.tasks.mux/org.ruminaq.tasks.mux.gui/src/main/java/org/ruminaq/tasks/mux/AddFeature.java
+++ bundles/org.ruminaq.tasks.mux/org.ruminaq.tasks.mux.gui/src/main/java/org/ruminaq/tasks/mux/AddFeature.java
@@ -1,9 +1,14 @@
-package org.ruminaq.tasks.mux.features;
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
+package org.ruminaq.tasks.mux;
 
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.ruminaq.gui.features.add.AbstractAddTaskFeature;
 import org.ruminaq.model.desc.PortsDescr;
-import org.ruminaq.tasks.mux.Images;
 import org.ruminaq.tasks.mux.impl.Port;
 
 public class AddFeature extends AbstractAddTaskFeature {
