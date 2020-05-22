a7b85d758def080254784d37d317a7b09cccf485
Marek Jagielski <marek.jagielski@gmail.com>
2020-05-22 20:59:48 +0200

    progress

--- bundles/org.ruminaq.tasks.demux/org.ruminaq.tasks.demux.gui/src/main/java/org/ruminaq/tasks/demux/gui/CreateFeaturesImpl.java
+++ bundles/org.ruminaq.tasks.demux/org.ruminaq.tasks.demux.gui/src/main/java/org/ruminaq/tasks/demux/gui/CreateFeaturesImpl.java
@@ -1,6 +1,12 @@
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
 package org.ruminaq.tasks.demux.gui;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.graphiti.features.ICreateFeature;
@@ -16,14 +22,22 @@ import org.ruminaq.tasks.demux.impl.Port;
 import org.ruminaq.tasks.demux.model.demux.Demux;
 import org.ruminaq.tasks.demux.model.demux.DemuxFactory;
 
+/**
+ * Service CreateFeaturesExtension implementation.
+ * 
+ * @author Marek Jagielski
+ */
 @Component(property = { "service.ranking:Integer=10" })
 public class CreateFeaturesImpl implements CreateFeaturesExtension {
 
   @Override
   public List<Class<? extends ICreateFeature>> getFeatures() {
-    return Arrays.asList(CreateFeature.class);
+    return Collections.singletonList(CreateFeature.class);
   }
-
+  
+  /**
+   * Demux create feature.
+   */
   public static class CreateFeature extends AbstractCreateTaskFeature
       implements PaletteCreateFeature {
 
