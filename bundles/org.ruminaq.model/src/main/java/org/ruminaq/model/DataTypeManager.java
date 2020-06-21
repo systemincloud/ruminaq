b3e55eefa883df7a77686250e3abe138ecc0dfcb
Marek Jagielski <marek.jagielski@gmail.com>
2020-06-21 23:59:46 +0200

    progress

--- bundles/org.ruminaq.model/src/main/java/org/ruminaq/model/DataTypeManager.java
+++ bundles/org.ruminaq.model/src/main/java/org/ruminaq/model/DataTypeManager.java
@@ -1,7 +1,5 @@
 package org.ruminaq.model;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,9 +8,6 @@ import java.util.Map.Entry;
 import java.util.Optional;
 
 import org.javatuples.Pair;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.osgi.service.component.annotations.Reference;
 import org.ruminaq.logs.ModelerLoggerFactory;
 import org.ruminaq.model.api.ModelExtensionHandler;
@@ -32,7 +27,6 @@ import org.ruminaq.model.ruminaq.DataType;
 
 import ch.qos.logback.classic.Logger;
 
-@SuppressWarnings("unchecked")
 public enum DataTypeManager {
   INSTANCE;
 
@@ -42,37 +36,7 @@ public enum DataTypeManager {
   @Reference
   private ModelExtensionHandler extensions;
 
-  private JSONParser parser = new JSONParser();
-
   private Map<Class<? extends DataType>, List<Pair<Class<? extends DataType>, Boolean>>> canCast = new HashMap<>();
-  {
-    JSONObject listJson = null;
-
-    try {
-      listJson = (JSONObject) parser.parse(new InputStreamReader(
-          DataTypeManager.class.getResourceAsStream("canCast.json")));
-
-      String pkg = Bool.class.getPackage().getName();
-
-      for (Object data : listJson.keySet()) {
-        Class<? extends DataType> dataClass = (Class<? extends DataType>) Class
-            .forName(pkg + "." + data);
-        canCast.put(dataClass,
-            new LinkedList<Pair<Class<? extends DataType>, Boolean>>());
-        JSONObject casts = (JSONObject) listJson.get(data);
-        for (Object castData : casts.keySet()) {
-          Class<? extends DataType> castClass = (Class<? extends DataType>) Class
-              .forName(pkg + "." + castData);
-          boolean notLossy = casts.get(castData).equals("t");
-          canCast.get(dataClass)
-              .add(new Pair<Class<? extends DataType>, Boolean>(castClass,
-                  notLossy));
-        }
-      }
-    } catch (IOException | ParseException | ClassNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
 
   public List<Class<? extends DataType>> getDataTypes() {
     List<Class<? extends DataType>> dts = new LinkedList<>();
