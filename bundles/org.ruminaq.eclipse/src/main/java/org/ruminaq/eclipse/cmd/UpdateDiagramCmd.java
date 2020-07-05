c51a48adab3b3ef5c8c5d6cc59e828969e72bb4a
Marek Jagielski <marek.jagielski@gmail.com>
2020-07-05 14:00:55 +0200

    progress

--- bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/cmd/UpdateDiagramCmd.java
+++ bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/cmd/UpdateDiagramCmd.java
@@ -6,11 +6,12 @@
 
 package org.ruminaq.eclipse.cmd;
 
+import java.util.Optional;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -18,17 +19,20 @@ public class UpdateDiagramCmd extends AbstractHandler {
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    ISelection selection = HandlerUtil.getCurrentSelection(event);
-    if (selection != null & selection instanceof IStructuredSelection) {
-      IStructuredSelection strucSelection = (IStructuredSelection) selection;
-      IResource file = (IResource) strucSelection.getFirstElement();
-      try {
-        new UpdateDiagram().updateDiagram(file);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-    return null;
+    Optional.of(event).map(HandlerUtil::getCurrentSelection)
+        .filter(IStructuredSelection.class::isInstance)
+        .map(IStructuredSelection.class::cast)
+        .map(IStructuredSelection::getFirstElement)
+        .filter(IResource.class::isInstance).map(IResource.class::cast)
+        .ifPresent((IResource file) -> {
+          try {
+            new UpdateDiagram().updateDiagram(file);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        });
+    return event;
+
   }
 
 }
