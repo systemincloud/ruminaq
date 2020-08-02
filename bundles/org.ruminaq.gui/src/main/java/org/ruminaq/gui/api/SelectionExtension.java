a507c9f4a48c24a00a81807e8d922e86fd02c357
Marek Jagielski <marek.jagielski@gmail.com>
2020-08-03 00:41:09 +0200

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/api/SelectionExtension.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/api/SelectionExtension.java
@@ -15,8 +15,20 @@ import org.eclipse.graphiti.mm.pictograms.PictogramElement;
  */
 public interface SelectionExtension {
 
+  /**
+   * Should use this extension.
+   *
+   * @param selection actually selected PictogramElement
+   * @return execute extension
+   */
   boolean forPictogramElement(PictogramElement selection);
 
+  /**
+   * Update array of selected PictogramElements.
+   *
+   * @param selection actually selected PictogramElement
+   * @return updated array
+   */
   PictogramElement[] getSelections(PictogramElement selection);
 
 }
