a6965af84c2d03405cf6ad0befa01177554945cf
Marek Jagielski <marek.jagielski@gmail.com>
2020-08-03 22:13:18 +0200

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/copy/CopyFeaturesImpl.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/copy/CopyFeaturesImpl.java
@@ -6,12 +6,11 @@
 
 package org.ruminaq.gui.features.copy;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
-
 import org.eclipse.graphiti.features.ICopyFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IContext;
@@ -23,7 +22,7 @@ import org.ruminaq.gui.model.diagram.SimpleConnectionPointShape;
 
 /**
  * Service CopyFeatureExtension implementation.
- * 
+ *
  * @author Marek Jagielski
  */
 @Component(property = { "service.ranking:Integer=5" })
@@ -31,7 +30,7 @@ public class CopyFeaturesImpl implements CopyFeatureExtension {
 
   @Override
   public List<Class<? extends ICopyFeature>> getFeatures() {
-    return Arrays.asList(CopyElementFeature.class);
+    return Collections.singletonList(CopyElementFeature.class);
   }
 
   @Override
@@ -40,8 +39,8 @@ public class CopyFeaturesImpl implements CopyFeatureExtension {
     return (Class<? extends ICopyFeature> clazz) -> Optional.of(context)
         .filter(ICopyContext.class::isInstance).map(ICopyContext.class::cast)
         .map(ICopyContext::getPictogramElements).map(Stream::of)
-        .orElse(Stream.empty())
+        .orElseGet(Stream::empty)
         .filter(Predicate.not(SimpleConnectionPointShape.class::isInstance))
-        .filter(RuminaqShape.class::isInstance).findAny().isPresent();
+        .anyMatch(RuminaqShape.class::isInstance);
   }
 }
