cc9b8654a5f3eba9d36dcbf015fbcb5f42c2f88c
Marek Jagielski <marek.jagielski@gmail.com>
2020-08-17 23:53:04 +0200

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyInternalInputPortFilter.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyInternalInputPortFilter.java
@@ -3,22 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
-package org.ruminaq.tasks.properties;
 
-import org.eclipse.emf.ecore.EObject;
+package org.ruminaq.gui.properties;
+
+import java.util.Optional;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
-import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.AbstractPropertySectionFilter;
-import org.ruminaq.gui.model.diagram.LabelShape;
-import org.ruminaq.model.ruminaq.InternalOutputPort;
+import org.ruminaq.gui.model.diagram.InputPortShape;
 
-public class PropertyInternalOutputPortFilter
+public class PropertyInternalInputPortFilter
     extends AbstractPropertySectionFilter {
 
   @Override
   protected boolean accept(PictogramElement pe) {
-    EObject eObject = Graphiti.getLinkService()
-        .getBusinessObjectForLinkedPictogramElement(pe);
-    return eObject instanceof InternalOutputPort && !LabelShape.class.isInstance(pe);
+    return Optional.of(pe).filter(InputPortShape.class::isInstance).isPresent();
   }
 }
--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyInternalOutputPortFilter.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyInternalOutputPortFilter.java
@@ -3,22 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
-package org.ruminaq.tasks.properties;
+package org.ruminaq.gui.properties;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.AbstractPropertySectionFilter;
 import org.ruminaq.gui.model.diagram.LabelShape;
-import org.ruminaq.model.ruminaq.InternalInputPort;
+import org.ruminaq.model.ruminaq.InternalOutputPort;
 
-public class PropertyInternalInputPortFilter
+public class PropertyInternalOutputPortFilter
     extends AbstractPropertySectionFilter {
 
   @Override
   protected boolean accept(PictogramElement pe) {
     EObject eObject = Graphiti.getLinkService()
         .getBusinessObjectForLinkedPictogramElement(pe);
-    return eObject instanceof InternalInputPort && !LabelShape.class.isInstance(pe);
+    return eObject instanceof InternalOutputPort && !LabelShape.class.isInstance(pe);
   }
 }
