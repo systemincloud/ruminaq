8e21a17b8d99962a4d5eefa0fcb1fe85698efaab
Marek Jagielski <marek.jagielski@gmail.com>
2020-09-01 16:37:50 +0200

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/custom/InternalPortBreakpointPropertiesFeature.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/custom/InternalPortBreakpointPropertiesFeature.java
@@ -3,10 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 package org.ruminaq.gui.features.custom;
 
+import java.util.Optional;
+import java.util.stream.Stream;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.graphiti.features.IFeatureProvider;
@@ -21,8 +23,10 @@ import org.eclipse.jface.window.SameShellProvider;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PropertyDialogAction;
 import org.ruminaq.debug.InternalPortBreakpoint;
+import org.ruminaq.gui.model.diagram.InternalPortShape;
 import org.ruminaq.model.ruminaq.InternalPort;
 import org.ruminaq.util.EclipseUtil;
+import org.ruminaq.util.Result;
 
 public class InternalPortBreakpointPropertiesFeature
     extends AbstractCustomFeature {
@@ -38,35 +42,45 @@ public class InternalPortBreakpointPropertiesFeature
     return NAME;
   }
 
+  private static Optional<InternalPortShape> shapeFromContext(
+      ICustomContext context) {
+    return Optional.ofNullable(context)
+        .map(ICustomContext::getPictogramElements).map(Stream::of)
+        .orElseGet(Stream::empty).findFirst()
+        .filter(InternalPortShape.class::isInstance)
+        .map(InternalPortShape.class::cast);
+  }
+
+  private static Optional<InternalPort> modelFromContext(
+      ICustomContext context) {
+    return shapeFromContext(context).map(InternalPortShape::getModelObject)
+        .filter(InternalPort.class::isInstance).map(InternalPort.class::cast);
+  }
+
+  private static Optional<IBreakpoint> breakpointFromContext(
+      ICustomContext context, IFeatureProvider fp) {
+    IResource resource = EclipseUtil.emfResourceToIResource(
+        fp.getDiagramTypeProvider().getDiagram().eResource());
+    InternalPort ip = modelFromContext(context).orElseThrow();
+    return Stream
+        .of(DebugPlugin.getDefault().getBreakpointManager()
+            .getBreakpoints(InternalPortBreakpoint.ID))
+        .filter(b -> resource.equals(b.getMarker().getResource()))
+        .filter(b -> ip.getTask().getId().equals(Result.attempt(
+            () -> b.getMarker().getAttribute(InternalPortBreakpoint.TASK_ID))
+            .orElse(null)))
+        .filter(b -> ip.getId().equals(Result.attempt(
+            () -> b.getMarker().getAttribute(InternalPortBreakpoint.PORT_ID))
+            .orElse(null)))
+        .findFirst();
+  }
+
   @Override
   public boolean isAvailable(IContext context) {
-    IResource resource = EclipseUtil.emfResourceToIResource(
-        getFeatureProvider().getDiagramTypeProvider().getDiagram().eResource());
-    Object bo = getFeatureProvider().getBusinessObjectForPictogramElement(
-        ((ICustomContext) context).getPictogramElements()[0]);
-
-    try {
-      if (bo != null && bo instanceof InternalPort) {
-        InternalPort ip = (InternalPort) bo;
-        IBreakpoint[] breakpoints = DebugPlugin.getDefault()
-            .getBreakpointManager().getBreakpoints(InternalPortBreakpoint.ID);
-        for (int i = 0; i < breakpoints.length; i++) {
-          IBreakpoint breakpoint = breakpoints[i];
-          if (resource.equals(breakpoint.getMarker().getResource())) {
-            if (breakpoint.getMarker()
-                .getAttribute(InternalPortBreakpoint.TASK_ID)
-                .equals(ip.getTask().getId())
-                && breakpoint.getMarker()
-                    .getAttribute(InternalPortBreakpoint.PORT_ID)
-                    .equals(ip.getId())) {
-              return true;
-            }
-          }
-        }
-      }
-    } catch (CoreException e) {
-    }
-    return false;
+    return breakpointFromContext(
+        Optional.of(context).filter(ICustomContext.class::isInstance)
+            .map(ICustomContext.class::cast).orElse(null),
+        getFeatureProvider()).isPresent();
   }
 
   @Override
@@ -85,50 +99,32 @@ public class InternalPortBreakpointPropertiesFeature
   }
 
   public static void doExecute(ICustomContext context, IFeatureProvider fp) {
-    IResource resource = EclipseUtil.emfResourceToIResource(
-        fp.getDiagramTypeProvider().getDiagram().eResource());
-    Object bo = fp.getBusinessObjectForPictogramElement(
-        ((ICustomContext) context).getPictogramElements()[0]);
+    Optional<IBreakpoint> bp = breakpointFromContext(
+        Optional.of(context).filter(ICustomContext.class::isInstance)
+            .map(ICustomContext.class::cast).orElse(null),
+        fp);
 
-    try {
-      if (bo != null && bo instanceof InternalPort) {
-        InternalPort ip = (InternalPort) bo;
-        IBreakpoint[] breakpoints = DebugPlugin.getDefault()
-            .getBreakpointManager().getBreakpoints(InternalPortBreakpoint.ID);
-        for (int i = 0; i < breakpoints.length; i++) {
-          final IBreakpoint breakpoint = breakpoints[i];
-          if (resource.equals(breakpoint.getMarker().getResource())) {
-            if (breakpoint.getMarker()
-                .getAttribute(InternalPortBreakpoint.TASK_ID)
-                .equals(ip.getTask().getId())
-                && breakpoint.getMarker()
-                    .getAttribute(InternalPortBreakpoint.PORT_ID)
-                    .equals(ip.getId())) {
-              PropertyDialogAction action = new PropertyDialogAction(
-                  new SameShellProvider(PlatformUI.getWorkbench()
-                      .getActiveWorkbenchWindow().getShell()),
-                  new ISelectionProvider() {
-                    public void addSelectionChangedListener(
-                        ISelectionChangedListener listener) {
-                    }
-
-                    public ISelection getSelection() {
-                      return new StructuredSelection(breakpoint);
-                    }
-
-                    public void removeSelectionChangedListener(
-                        ISelectionChangedListener listener) {
-                    }
-
-                    public void setSelection(ISelection selection) {
-                    }
-                  });
-              action.run();
+    if (bp.isPresent()) {
+      PropertyDialogAction action = new PropertyDialogAction(
+          new SameShellProvider(
+              PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()),
+          new ISelectionProvider() {
+            public void addSelectionChangedListener(
+                ISelectionChangedListener listener) {
             }
-          }
-        }
-      }
-    } catch (CoreException e) {
+
+            public ISelection getSelection() {
+              return new StructuredSelection(bp.get());
+            }
+
+            public void removeSelectionChangedListener(
+                ISelectionChangedListener listener) {
+            }
+
+            public void setSelection(ISelection selection) {
+            }
+          });
+      action.run();
     }
   }
 }
