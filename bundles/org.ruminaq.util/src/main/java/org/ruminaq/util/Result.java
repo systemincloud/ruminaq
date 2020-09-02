ca34988e82494f570bb1964268ea0f1478603ac1
Marek Jagielski <marek.jagielski@gmail.com>
2020-09-02 22:23:51 +0200

    progress

--- bundles/org.ruminaq.util/src/main/java/org/ruminaq/util/Result.java
+++ bundles/org.ruminaq.util/src/main/java/org/ruminaq/util/Result.java
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 package org.ruminaq.util;
 
 import java.util.Objects;
@@ -10,6 +11,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 /**
+ * Helper class to wrap Errors in streams.
  *
  * @author Brian Gerstle, Marek Jagielski
  *
