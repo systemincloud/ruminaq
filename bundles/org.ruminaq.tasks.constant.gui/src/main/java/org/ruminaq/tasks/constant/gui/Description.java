16bff5d4d9e92a33a423837f10e8aedb79f6f07b
Marek Jagielski <marek.jagielski@gmail.com>
2020-09-10 23:21:17 +0200

    progress

---
+++ bundles/org.ruminaq.tasks.constant.gui/src/main/java/org/ruminaq/tasks/constant/gui/Description.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
+package org.ruminaq.tasks.constant.gui;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+import org.osgi.service.component.annotations.Component;
+import org.ruminaq.gui.api.PropertyDescriptionExtension;
+import org.ruminaq.tasks.constant.gui.Description.Filter;
+import org.ruminaq.tasks.constant.model.constant.Constant;
+import org.ruminaq.util.ServiceFilter;
+import org.ruminaq.util.ServiceFilterArgs;
+
+@Component(property = { "service.ranking:Integer=5" })
+@ServiceFilter(Filter.class)
+public class Description implements PropertyDescriptionExtension {
+
+  /**
+   * Only on Constant.
+   */
+  public static class Filter implements Predicate<ServiceFilterArgs> {
+
+    @Override
+    public boolean test(ServiceFilterArgs args) {
+      return Optional.ofNullable(args).map(ServiceFilterArgs::getArgs)
+          .map(l -> l.get(0)).filter(Constant.class::isInstance).isPresent();
+    }
+  }
+
+  @Override
+  public String getDescription() {
+    return "XXX";
+  }
+
+}
