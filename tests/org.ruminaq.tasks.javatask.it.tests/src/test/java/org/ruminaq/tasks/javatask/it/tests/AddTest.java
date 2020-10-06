4afc89ede7b5a85d89687372d624d007cb58622c
Marek Jagielski <marek.jagielski@gmail.com>
2020-10-06 11:39:28 +0200

    progress

--- tests/org.ruminaq.tasks.javatask.it.tests/src/test/java/org/ruminaq/tasks/javatask/it/tests/AddTest.java
+++ tests/org.ruminaq.tasks.javatask.it.tests/src/test/java/org/ruminaq/tasks/javatask/it/tests/AddTest.java
@@ -33,8 +33,8 @@ public class AddTest extends GuiTest {
 
   @BeforeClass
   public static void initBot() {
-    bot = new SWTWorkbenchBot();
     SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+    bot = new SWTWorkbenchBot();
   }
 
   @AfterClass
