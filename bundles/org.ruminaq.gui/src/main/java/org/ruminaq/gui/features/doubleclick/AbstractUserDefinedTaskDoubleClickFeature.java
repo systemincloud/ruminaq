d89df903b68aacf3b3e335f591cfa06c79c3c6a5
Marek Jagielski <marek.jagielski@gmail.com>
2020-10-24 21:24:43 +0200

    progress

---
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/features/doubleclick/AbstractUserDefinedTaskDoubleClickFeature.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
+package org.ruminaq.gui.features.doubleclick;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.IContext;
+import org.eclipse.graphiti.features.context.ICustomContext;
+import org.eclipse.graphiti.features.context.IDoubleClickContext;
+import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+import org.ruminaq.gui.model.diagram.RuminaqShape;
+
+/**
+ * UserDefinedTask DoubleClickFeature.
+ *
+ * @author Marek Jagielski
+ */
+public abstract class AbstractUserDefinedTaskDoubleClickFeature
+    extends AbstractCustomFeature {
+
+  public AbstractUserDefinedTaskDoubleClickFeature(IFeatureProvider fp) {
+    super(fp);
+  }
+
+  protected static <T> Optional<T> toModel(IContext context, Class<T> type) {
+    return Optional.of(context).filter(IDoubleClickContext.class::isInstance)
+        .map(IDoubleClickContext.class::cast)
+        .map(IDoubleClickContext::getPictogramElements).map(Stream::of)
+        .orElseGet(Stream::empty).findFirst()
+        .filter(RuminaqShape.class::isInstance).map(RuminaqShape.class::cast)
+        .map(RuminaqShape::getModelObject).filter(type::isInstance)
+        .map(type::cast);
+  }
+
+  @Override
+  public boolean canExecute(ICustomContext context) {
+    return true;
+  }
+
+  @Override
+  public boolean hasDoneChanges() {
+    return false;
+  }
+}
--- bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/DoubleClickFeature.java
+++ bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/DoubleClickFeature.java
@@ -9,14 +9,11 @@ package org.ruminaq.tasks.javatask.gui;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IContext;
 import org.eclipse.graphiti.features.context.ICustomContext;
-import org.eclipse.graphiti.features.context.IDoubleClickContext;
-import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
@@ -30,7 +27,7 @@ import org.eclipse.jdt.core.search.SearchRequestor;
 import org.eclipse.swt.widgets.Display;
 import org.ruminaq.gui.features.FeatureFilter;
 import org.ruminaq.gui.features.FeaturePredicate;
-import org.ruminaq.gui.model.diagram.RuminaqShape;
+import org.ruminaq.gui.features.doubleclick.AbstractUserDefinedTaskDoubleClickFeature;
 import org.ruminaq.tasks.javatask.model.javatask.JavaTask;
 import org.ruminaq.util.EclipseUtil;
 import org.ruminaq.util.Result;
@@ -41,39 +38,19 @@ import org.ruminaq.util.Result;
  * @author Marek Jagielski
  */
 @FeatureFilter(DoubleClickFeature.Filter.class)
-public class DoubleClickFeature extends AbstractCustomFeature {
+public class DoubleClickFeature extends AbstractUserDefinedTaskDoubleClickFeature {
 
   private IType type;
 
   public static class Filter implements FeaturePredicate<IContext> {
     @Override
     public boolean test(IContext context) {
-      return toModel(context).isPresent();
+      return toModel(context, JavaTask.class).isPresent();
     }
   }
 
   public DoubleClickFeature(IFeatureProvider fp) {
     super(fp);
-  }
-
-  private static Optional<JavaTask> toModel(IContext context) {
-    return Optional.of(context).filter(IDoubleClickContext.class::isInstance)
-        .map(IDoubleClickContext.class::cast)
-        .map(IDoubleClickContext::getPictogramElements).map(Stream::of)
-        .orElseGet(Stream::empty).findFirst()
-        .filter(RuminaqShape.class::isInstance).map(RuminaqShape.class::cast)
-        .map(RuminaqShape::getModelObject).filter(JavaTask.class::isInstance)
-        .map(JavaTask.class::cast);
-  }
-
-  @Override
-  public boolean canExecute(ICustomContext context) {
-    return true;
-  }
-
-  @Override
-  public boolean hasDoneChanges() {
-    return false;
   }
 
   /**
@@ -87,7 +64,7 @@ public class DoubleClickFeature extends AbstractCustomFeature {
         .createJavaSearchScope(new IJavaElement[] {
             JavaCore.create(ResourcesPlugin.getWorkspace().getRoot().getProject(
                 EclipseUtil.getProjectNameFromDiagram(getDiagram()))) });
-    toModel(context).map(JavaTask::getImplementationPath)
+    toModel(context, JavaTask.class).map(JavaTask::getImplementationPath)
         .filter(Predicate.not(""::equals))
         .map(c -> SearchPattern.createPattern(c, IJavaSearchConstants.TYPE,
             IJavaSearchConstants.TYPE,
