bb748973fb563c73f977382be356084057fd8fa2
Marek Jagielski <marek.jagielski@gmail.com>
2020-11-06 17:48:41 +0100

    progress

--- bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/wizards/CreateJavaTaskPage.java
+++ bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/wizards/CreateJavaTaskPage.java
@@ -68,7 +68,7 @@ import org.slf4j.Logger;
  * JavaTask wizard page.
  * 
  * <p>Extends UI part from AbstractCreateUserDefinedTaskPage. Implements java
- * class generation.
+ * class generation.</p>
  *
  * <pre>
  * @JavaTaskInfo public class MyTask extends JavaTask {
