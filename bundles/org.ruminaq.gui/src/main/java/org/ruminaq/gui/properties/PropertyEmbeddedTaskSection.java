437b6200f25f85bf19ba4e9d4e811012b1d73a29
Marek Jagielski <marek.jagielski@gmail.com>
2020-11-14 23:32:31 +0100

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyEmbeddedTaskSection.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/PropertyEmbeddedTaskSection.java
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
 
-package org.ruminaq.tasks.embeddedtask;
+package org.ruminaq.gui.properties;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -21,14 +21,11 @@ import org.eclipse.graphiti.dt.IDiagramTypeProvider;
 import org.eclipse.graphiti.features.context.impl.UpdateContext;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -46,19 +43,14 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
 import org.eclipse.ui.dialogs.ISelectionStatusValidator;
-import org.eclipse.ui.model.BaseWorkbenchContentProvider;
-import org.eclipse.ui.model.WorkbenchLabelProvider;
-import org.eclipse.ui.wizards.IWizardDescriptor;
 import org.ruminaq.eclipse.RuminaqDiagramUtil;
-import org.ruminaq.eclipse.wizards.diagram.CreateDiagramWizard;
-import org.ruminaq.eclipse.wizards.diagram.CreateTestDiagramWizard;
 import org.ruminaq.eclipse.wizards.project.SourceFolders;
 import org.ruminaq.gui.features.update.UpdateEmbeddedTaskFeature;
 import org.ruminaq.model.ruminaq.EmbeddedTask;
 import org.ruminaq.model.ruminaq.ModelUtil;
 import org.ruminaq.util.EclipseUtil;
 
-public class PropertySection {
+public class PropertyEmbeddedTaskSection {
 
   public static final String MAIN_PREFIX = "main:";
   public static final String TEST_PREFIX = "test:";
@@ -74,7 +66,7 @@ public class PropertySection {
   private TransactionalEditingDomain ed;
   private IDiagramTypeProvider dtp;
 
-  public PropertySection(Composite parent, PictogramElement pe,
+  public PropertyEmbeddedTaskSection(Composite parent, PictogramElement pe,
       TransactionalEditingDomain ed, IDiagramTypeProvider dtp) {
     String path = EclipseUtil.getModelPathFromEObject(pe).toString();
     boolean test = RuminaqDiagramUtil
@@ -167,154 +159,154 @@ public class PropertySection {
       public void widgetSelected(SelectionEvent evt) {
         Shell shell = txtTaskName.getShell();
 
-        ElementTreeSelectionDialog fileDialog = new ElementTreeSelectionDialog(
-            shell, new WorkbenchLabelProvider(),
-            new BaseWorkbenchContentProvider());
-        if (test)
-          fileDialog.setInput(ResourcesPlugin.getWorkspace().getRoot()
-              .getProject(
-                  EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))
-              .getFolder("src"));
-        else
-          fileDialog.setInput(ResourcesPlugin.getWorkspace().getRoot()
-              .getProject(
-                  EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))
-              .getFolder(SourceFolders.MAIN_RESOURCES));
-        fileDialog.setTitle("Select Diagram File");
-        fileDialog.setMessage("Select diagram file from the tree:");
-        fileDialog.setAllowMultiple(false);
-        fileDialog.addFilter(new ViewerFilter() {
-          @Override
-          public boolean select(Viewer arg0, Object parent, Object element) {
-            if (element instanceof IProject)
-              return true;
+//        ElementTreeSelectionDialog fileDialog = new ElementTreeSelectionDialog(
+//            shell, new WorkbenchLabelProvider(),
+//            new BaseWorkbenchContentProvider());
+//        if (test)
+//          fileDialog.setInput(ResourcesPlugin.getWorkspace().getRoot()
+//              .getProject(
+//                  EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))
+//              .getFolder("src"));
+//        else
+//          fileDialog.setInput(ResourcesPlugin.getWorkspace().getRoot()
+//              .getProject(
+//                  EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))
+//              .getFolder(SourceFolders.MAIN_RESOURCES));
+//        fileDialog.setTitle("Select Diagram File");
+//        fileDialog.setMessage("Select diagram file from the tree:");
+//        fileDialog.setAllowMultiple(false);
+//        fileDialog.addFilter(new ViewerFilter() {
+//          @Override
+//          public boolean select(Viewer arg0, Object parent, Object element) {
+//            if (element instanceof IProject)
+//              return true;
+//
+//            if (element instanceof IFolder) {
+//              IPath dirs = ((IFolder) element).getProjectRelativePath();
+//              IPath mainPath = new Path(SourceFolders.MAIN_RESOURCES);
+//              for (int i = 1; i < mainPath.segmentCount(); i++)
+//                if (dirs.equals(mainPath.uptoSegment(i)))
+//                  return true;
+//              if (dirs.matchingFirstSegments(mainPath) == 3)
+//                return true;
+//              IPath testPath = new Path(SourceFolders.TEST_RESOURCES);
+//              for (int i = 1; i < testPath.segmentCount(); i++)
+//                if (dirs.equals(testPath.uptoSegment(i)))
+//                  return true;
+//              if (dirs.matchingFirstSegments(testPath) == 3)
+//                return true;
+//              return false;
+//            }
+//
+//            if (element instanceof IFile) {
+//              if (((IFile) element).getFileExtension() == null)
+//                return false;
+//              if (((IFile) element).getFullPath().toString().equals(path))
+//                return false;
+//
+//              String ext = ((IFile) element).getFileExtension();
+////              if (ext.equals(CreateDiagramWizard.EXTENSION))
+//                return true;
+//            }
+//            return false;
+//          }
 
-            if (element instanceof IFolder) {
-              IPath dirs = ((IFolder) element).getProjectRelativePath();
-              IPath mainPath = new Path(SourceFolders.MAIN_RESOURCES);
-              for (int i = 1; i < mainPath.segmentCount(); i++)
-                if (dirs.equals(mainPath.uptoSegment(i)))
-                  return true;
-              if (dirs.matchingFirstSegments(mainPath) == 3)
-                return true;
-              IPath testPath = new Path(SourceFolders.TEST_RESOURCES);
-              for (int i = 1; i < testPath.segmentCount(); i++)
-                if (dirs.equals(testPath.uptoSegment(i)))
-                  return true;
-              if (dirs.matchingFirstSegments(testPath) == 3)
-                return true;
-              return false;
-            }
-
-            if (element instanceof IFile) {
-              if (((IFile) element).getFileExtension() == null)
-                return false;
-              if (((IFile) element).getFullPath().toString().equals(path))
-                return false;
-
-              String ext = ((IFile) element).getFileExtension();
-              if (ext.equals(CreateDiagramWizard.EXTENSION))
-                return true;
-            }
-            return false;
-          }
-
-        });
-        fileDialog.setValidator(new ISelectionStatusValidator() {
-          @Override
-          public IStatus validate(Object[] selection) {
+//        });
+//        fileDialog.setValidator(new ISelectionStatusValidator() {
+//          @Override
+//          public IStatus validate(Object[] selection) {
 //            if (selection.length == 1 && selection[0] instanceof IFile)
 //              return new Status(IStatus.OK, SicPlugin.GUI_ID.s(), 0, "", null);
 //            else
 //              return new Status(IStatus.ERROR, SicPlugin.GUI_ID.s(), 0, "",
 //                  null);
-            return null;
-          }
-        });
-        fileDialog.open();
-        Object[] results = fileDialog.getResult();
+//            return null;
+//          }
+//        });
+//        fileDialog.open();
+//        Object[] results = fileDialog.getResult();
 
-        if (results != null) {
-          final IPath selectedPath = ((IFile) results[0]).getFullPath()
-              .removeFirstSegments(1);
-          String taskPath = ((IFile) results[0]).getFullPath()
-              .removeFirstSegments(5).toString();
-          String show = "";
-          if (selectedPath.toString().startsWith(SourceFolders.MAIN_RESOURCES))
-            show = MAIN_PREFIX + taskPath;
-          if (selectedPath.toString().startsWith(SourceFolders.TEST_RESOURCES))
-            show = TEST_PREFIX + taskPath;
-
-          if (taskPath != null)
-            txtTaskName.setText(show);
-
-          ModelUtil.runModelChange(new Runnable() {
-            @Override
-            public void run() {
-              String implementationPath = txtTaskName.getText();
-              if (implementationPath == null)
-                return;
-
-              EObject bo = Graphiti.getLinkService()
-                  .getBusinessObjectForLinkedPictogramElement(pe);
-              if (bo == null || !(bo instanceof EmbeddedTask))
-                return;
-              EmbeddedTask et = (EmbeddedTask) bo;
-
-              et.setImplementationTask(selectedPath.toString());
-              UpdateContext context = new UpdateContext(pe);
-              dtp.getFeatureProvider().updateIfPossible(context);
-            }
-          }, ed, "Change embedded diagram");
-        }
+//        if (results != null) {
+//          final IPath selectedPath = ((IFile) results[0]).getFullPath()
+//              .removeFirstSegments(1);
+//          String taskPath = ((IFile) results[0]).getFullPath()
+//              .removeFirstSegments(5).toString();
+//          String show = "";
+//          if (selectedPath.toString().startsWith(SourceFolders.MAIN_RESOURCES))
+//            show = MAIN_PREFIX + taskPath;
+//          if (selectedPath.toString().startsWith(SourceFolders.TEST_RESOURCES))
+//            show = TEST_PREFIX + taskPath;
+//
+//          if (taskPath != null)
+//            txtTaskName.setText(show);
+//
+//          ModelUtil.runModelChange(new Runnable() {
+//            @Override
+//            public void run() {
+//              String implementationPath = txtTaskName.getText();
+//              if (implementationPath == null)
+//                return;
+//
+//              EObject bo = Graphiti.getLinkService()
+//                  .getBusinessObjectForLinkedPictogramElement(pe);
+//              if (bo == null || !(bo instanceof EmbeddedTask))
+//                return;
+//              EmbeddedTask et = (EmbeddedTask) bo;
+//
+//              et.setImplementationTask(selectedPath.toString());
+//              UpdateContext context = new UpdateContext(pe);
+//              dtp.getFeatureProvider().updateIfPossible(context);
+//            }
+//          }, ed, "Change embedded diagram");
+//        }
       }
     });
     btnNewDiagram.addSelectionListener(new SelectionAdapter() {
       @Override
       public void widgetSelected(SelectionEvent evt) {
-        IWizardDescriptor descriptor = PlatformUI.getWorkbench()
-            .getNewWizardRegistry().findWizard(CreateDiagramWizard.ID);
-        try {
-          if (descriptor != null) {
-            IWizard wizard = descriptor.createWizard();
-            IStructuredSelection selection = new StructuredSelection(JavaCore
-                .create(ResourcesPlugin.getWorkspace().getRoot().getProject(
-                    EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))));
-            ((CreateDiagramWizard) wizard).init(PlatformUI.getWorkbench(),
-                selection);
-            WizardDialog wd = new WizardDialog(
-                Display.getDefault().getActiveShell(), wizard);
-            wd.setTitle(wizard.getWindowTitle());
-            wd.open();
-          }
-        } catch (CoreException e) {
-          e.printStackTrace();
-        }
+//        IWizardDescriptor descriptor = PlatformUI.getWorkbench()
+//            .getNewWizardRegistry().findWizard(CreateDiagramWizard.ID);
+//        try {
+//          if (descriptor != null) {
+//            IWizard wizard = descriptor.createWizard();
+//            IStructuredSelection selection = new StructuredSelection(JavaCore
+//                .create(ResourcesPlugin.getWorkspace().getRoot().getProject(
+//                    EclipseUtil.getProjectNameFromDiagram(dtp.getDiagram()))));
+//            ((CreateDiagramWizard) wizard).init(PlatformUI.getWorkbench(),
+//                selection);
+//            WizardDialog wd = new WizardDialog(
+//                Display.getDefault().getActiveShell(), wizard);
+//            wd.setTitle(wizard.getWindowTitle());
+//            wd.open();
+//          }
+//        } catch (CoreException e) {
+//          e.printStackTrace();
+//        }
       }
     });
     if (test)
       btnNewTest.addSelectionListener(new SelectionAdapter() {
         @Override
         public void widgetSelected(SelectionEvent evt) {
-          IWizardDescriptor descriptor = PlatformUI.getWorkbench()
-              .getNewWizardRegistry().findWizard(CreateTestDiagramWizard.ID);
-          try {
-            if (descriptor != null) {
-              IWizard wizard = descriptor.createWizard();
-              IStructuredSelection selection = new StructuredSelection(
-                  JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()
-                      .getProject(EclipseUtil
-                          .getProjectNameFromDiagram(dtp.getDiagram()))));
-              ((CreateTestDiagramWizard) wizard).init(PlatformUI.getWorkbench(),
-                  selection);
-              WizardDialog wd = new WizardDialog(
-                  Display.getDefault().getActiveShell(), wizard);
-              wd.setTitle(wizard.getWindowTitle());
-              wd.open();
-            }
-          } catch (CoreException e) {
-            e.printStackTrace();
-          }
+//          IWizardDescriptor descriptor = PlatformUI.getWorkbench()
+//              .getNewWizardRegistry().findWizard(CreateTestDiagramWizard.ID);
+//          try {
+//            if (descriptor != null) {
+//              IWizard wizard = descriptor.createWizard();
+//              IStructuredSelection selection = new StructuredSelection(
+//                  JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()
+//                      .getProject(EclipseUtil
+//                          .getProjectNameFromDiagram(dtp.getDiagram()))));
+//              ((CreateTestDiagramWizard) wizard).init(PlatformUI.getWorkbench(),
+//                  selection);
+//              WizardDialog wd = new WizardDialog(
+//                  Display.getDefault().getActiveShell(), wizard);
+//              wd.setTitle(wizard.getWindowTitle());
+//              wd.open();
+//            }
+//          } catch (CoreException e) {
+//            e.printStackTrace();
+//          }
         }
       });
   }
