d93866de4f85027b544e517dde100ba4c7d3de49
Marek Jagielski <marek.jagielski@gmail.com>
2020-12-06 11:25:09 +0100

    progress

--- bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/wizards/CreateProjectWizard.java
+++ bundles/org.ruminaq.tasks.javatask.gui/src/main/java/org/ruminaq/tasks/javatask/gui/wizards/CreateProjectWizard.java
@@ -23,8 +23,7 @@ public class CreateProjectWizard {
     return true;
   }
 
-  private static void createSourceFolders(IProject project)
-      throws CoreException {
+  private static void createSourceFolders(IProject project) {
     EclipseUtil.createFolderWithParents(project,
         EclipseExtensionImpl.MAIN_JAVA);
     EclipseUtil.createFileInFolder(project, EclipseExtensionImpl.MAIN_JAVA,
