71c9405483a993554775b2794ef0716071cbe36c
Marek Jagielski <marek.jagielski@gmail.com>
2021-01-01 15:05:08 +0100

    progress

--- bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/cmd/UpdateDiagram.java
+++ bundles/org.ruminaq.eclipse/src/main/java/org/ruminaq/eclipse/cmd/UpdateDiagram.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
