0f9109d63b616c6152bc7f8fdcf1258ee2560e8d
Marek Jagielski <marek.jagielski@gmail.com>
2021-01-16 00:32:58 +0100

    progress

--- bundles/org.ruminaq.gui.model/src/main/java/org/ruminaq/gui/model/diagram/impl/simpleconnection/ArrowDecorator.java
+++ bundles/org.ruminaq.gui.model/src/main/java/org/ruminaq/gui/model/diagram/impl/simpleconnection/ArrowDecorator.java
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 package org.ruminaq.gui.model.diagram.impl.simpleconnection;
 
 import org.eclipse.emf.common.util.BasicEList;
@@ -67,7 +68,7 @@ public class ArrowDecorator extends ConnectionDecoratorImpl {
 
   /**
    * Arrow on SimpleConnectionShape.
-   * 
+   *
    * @param shape parent SimpleConnectionShape
    */
   public ArrowDecorator(SimpleConnectionShape shape) {
