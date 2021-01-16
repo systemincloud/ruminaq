9fe0b3caaf5031356d376172c914850168da7e08
Marek Jagielski <marek.jagielski@gmail.com>
2021-01-16 16:37:03 +0100

    progress

--- bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/AbstractUserDefinedTaskPropertySection.java
+++ bundles/org.ruminaq.gui/src/main/java/org/ruminaq/gui/properties/AbstractUserDefinedTaskPropertySection.java
@@ -55,6 +55,7 @@ public abstract class AbstractUserDefinedTaskPropertySection
   }
 
   /**
+   * Layout.
    *                 _______________  ________ ________
    * Implementation: |              | |Select| |Create|
    *                 ```````````````` ```````` ````````
