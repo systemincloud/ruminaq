ca466d9bd60f31464c1f831a1cc9743e72e7376d
Marek Jagielski <marek.jagielski@gmail.com>
2021-03-02 20:53:54 +0100

    progress

--- tests/org.ruminaq.tasks.gate.it.tests/src/test/java/org/ruminaq/tasks/gate/it/tests/AddTest.java
+++ tests/org.ruminaq.tasks.gate.it.tests/src/test/java/org/ruminaq/tasks/gate/it/tests/AddTest.java
@@ -3,12 +3,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 package org.ruminaq.tasks.gate.it.tests;
 
 import static org.junit.Assert.assertEquals;
+import org.eclipse.reddeer.eclipse.ui.views.properties.PropertySheet;
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ruminaq.gui.model.diagram.InternalInputPortShape;
 import org.ruminaq.gui.model.diagram.InternalOutputPortShape;
 import org.ruminaq.model.ruminaq.InternalInputPort;
 import org.ruminaq.model.ruminaq.InternalOutputPort;
@@ -44,7 +47,13 @@ public class AddTest extends GuiTest {
 
   @Test
   public void testAddXor() throws InterruptedException {
-    addToolFromPalette("Or", 200, 100);
+    addToolFromPalette("Xor", 200, 100);
+
+    new WithShapeGraphitiEditPart(InternalInputPortShape.class).select();
+    PropertySheet propertiesView = new PropertySheet();
+    propertiesView.open();
+    propertiesView.activate();
+    propertiesView.selectTab("Internal Input Port");
   }
 
   @Test
@@ -63,6 +72,7 @@ public class AddTest extends GuiTest {
         new WithBoGraphitiEditPart(And.class, InternalOutputPort.class),
         new WithBoGraphitiEditPart(Not.class, InternalInputPort.class))
             .execute();
-    assertDiagram(diagramEditor, "AddTest.testAddSimpleConnectionBetweenTasks.xml");
+    assertDiagram(diagramEditor,
+        "AddTest.testAddSimpleConnectionBetweenTasks.xml");
   }
 }
